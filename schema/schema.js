1801769d03e62a224ce85873f928fa9b5e6b36ea
kevinccbsg <kevinccbsg@gmail.com>
2017-09-26 01:29:20 +0200

    Añadidas resolver para el caso de company con user y las demas queries

--- schema/schema.js
+++ schema/schema.js
@@ -2,20 +2,50 @@ const axios = require('axios')
 const { makeExecutableSchema } = require('graphql-tools')
 
 const typeDefs = `
-  type User {
+  type Company {
     id: String
+    name: String
+    description: String
+  }
+
+  type User {
+    id: String!
     firstName: String
     age: Int
+    company: Company
   }
 
   type Query {
-    user(id: String!): User
+    user(id: String): User
+    companies: [Company]
+    company(id: String): Company
   }
 `
 
 const resolvers = {
   Query: {
-    user: (_, { id }) => axios.get(`http://localhost:3000/user/${id}`)
+    user: (_, { id }) => {
+      console.log('user');
+      return axios.get(`http://localhost:3000/users/${id}`)
+      .then(resp => resp.data)
+    },
+    companies: () => {
+      console.log('comapnies');
+      return axios.get('http://localhost:3000/companies')
+      .then(resp => resp.data)
+    },
+    company: (_, args) => {
+      console.log(_, args);
+      return axios.get(`http://localhost:3000/companies/${args.id}`)
+      .then(resp => resp.data)
+    },
+  },
+  User: {
+    company: (_, args) => {
+      console.log(_, args);
+      return axios.get(`http://localhost:3000/companies/${_.companyId}`)
+      .then(resp => resp.data)
+    },
   }
 }
 
