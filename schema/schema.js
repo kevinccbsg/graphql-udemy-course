e240c9520f0e6e0c5fe7f41ea57d0551ae20c495
kevinccbsg <kevinccbsg@gmail.com>
2017-10-06 00:16:46 +0200

    Añadidos los resolver y esquemas para las mutaciones de creación, borrado y edición

--- schema/schema.js
+++ schema/schema.js
@@ -6,6 +6,7 @@ const typeDefs = `
     id: String
     name: String
     description: String
+    users: [User]
   }
 
   type User {
@@ -15,34 +16,61 @@ const typeDefs = `
     company: Company
   }
 
+  input NewUser {
+    firstName: String!
+    age: Int
+  }
+
   type Query {
     user(id: String): User
     companies: [Company]
     company(id: String): Company
+  }
+
+  type Mutation {
+    addUser(user: NewUser): User
+    deleteUser(id: ID!): User
+    updateUser(id: ID!, user: NewUser): User
   }
 `
 
 const resolvers = {
   Query: {
     user: (_, { id }) => {
-      console.log('user');
       return axios.get(`http://localhost:3000/users/${id}`)
       .then(resp => resp.data)
     },
     companies: () => {
-      console.log('comapnies');
       return axios.get('http://localhost:3000/companies')
       .then(resp => resp.data)
     },
     company: (_, args) => {
-      console.log(_, args);
       return axios.get(`http://localhost:3000/companies/${args.id}`)
       .then(resp => resp.data)
     },
   },
+  Mutation: {
+    addUser: (_, args) => {
+      return axios.post('http://localhost:3000/users/', args)
+      .then(resp => resp.data)
+    },
+    deleteUser: (_, { id }) => {
+      return axios.delete(`http://localhost:3000/users/${id}`)
+      .then(resp => resp.data)
+    },
+    updateUser: (_, { id, user }) => {
+      return axios.patch(`http://localhost:3000/users/${id}`, user)
+      .then(resp => resp.data)
+    },
+  },
+  Company: {
+    users: (_, args) => {
+      return axios.get(`http://localhost:3000/companies/${_.id}/users`)
+      .then(resp => resp.data);
+    },
+  },
   User: {
     company: (_, args) => {
-      console.log(_, args);
       return axios.get(`http://localhost:3000/companies/${_.companyId}`)
       .then(resp => resp.data)
     },
